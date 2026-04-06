e99f7cd45e182a3de2c23cc1600ad339c14237d9
salman <salmankhilji010@gmail.com>
2026-04-06 12:41:00 +0500

    Refactor: Remove duplocated styles

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react"
+
+interface props {
+    children: React.ReactNode
+}
+const GameCardContainer = ({ children }: props) => {
+  return (
+    <Box  width={'300px'} borderRadius={10} overflow={"hidden"}>{children}</Box>
+  )
+}
+
+export default GameCardContainer
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import useGames from "@/hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -12,14 +13,18 @@ const GameGrid = () => {
       {isLoading && (
         <SimpleGrid columns={{ base: 1, md: 2, lg: 3, xl: 4 }} padding={5} gap={10}>
           {Skeletons.map((skeleton) => (
-            <GameCardSkeleton key={skeleton} />
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
           ))}
         </SimpleGrid>
       )}
       <SimpleGrid columns={{ base: 1, md: 2, lg: 3, xl: 4 }} padding={5} gap={10}>
         {}
         {games?.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </div>
