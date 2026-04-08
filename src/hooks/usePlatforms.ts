cfae2ccb15c590fbb67501ee025f94ca3ab06d34
salman <salmankhilji010@gmail.com>
2026-04-08 19:41:59 +0500

    Build platform selector

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,14 @@
+import useData from "./useData";
+
+export interface Genre {
+    id: number;
+    name: string;
+    slug: string;
+}
+
+const usePlatforms = () => {
+    const usePlatforms = useData<Genre>("/platforms/lists/parents");
+    return usePlatforms;
+}
+
+export default usePlatforms
